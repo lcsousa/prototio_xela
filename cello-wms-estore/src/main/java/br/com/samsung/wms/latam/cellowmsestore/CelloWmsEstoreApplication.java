1f15f32e932ea81e508ec5054e354fa029e1726c
Paulo Roberto Baptista Valentim <paulo.valentim@meta.com.br>
2022-02-28 17:50:54 -0300

    Scheduler

--- cello-wms-estore/src/main/java/br/com/samsung/wms/latam/cellowmsestore/CelloWmsEstoreApplication.java
+++ cello-wms-estore/src/main/java/br/com/samsung/wms/latam/cellowmsestore/CelloWmsEstoreApplication.java
@@ -2,7 +2,9 @@ package br.com.samsung.wms.latam.cellowmsestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"br.com.samsung.wms.latam.cellowmsestore"})
 public class CelloWmsEstoreApplication {
 
